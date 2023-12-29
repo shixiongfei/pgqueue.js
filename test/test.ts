505068afbac3dc205cb022c693539edf68e9ec9c
shixiongfei <jenson.shixf@gmail.com>
2023-12-29 16:30:02 +0800

    Init project

---
+++ test/test.ts
@@ -0,0 +1,10 @@
+/*
+ * test.ts
+ *
+ * Copyright (c) 2023 Xiongfei Shi
+ *
+ * Author: Xiongfei Shi <xiongfei.shi(a)icloud.com>
+ * License: Apache-2.0
+ *
+ * https://github.com/shixiongfei/pgqueue.js
+ */
